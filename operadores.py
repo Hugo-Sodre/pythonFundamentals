b46c84fbeaf8ecd6dc5bd902e84ac17f9962dd88
Hugo Sodré <hugosodre031@gmail.com>
2023-08-08 01:08:19 -0300

    operadores

---
+++ operadores.py
@@ -0,0 +1,33 @@
+print (1+1)
+
+print(10-2)
+
+print(4 * 3)
+
+
+print(12 / 3)
+
+print(12 // 2)
+
+print(10 -5 * 2)
+
+print((10 -5) * 2)
+
+print(10 ** 2 * 2)
+
+print(10 ** (2 *2))
+
+print(10 /2 * 4)
+
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 * produto_2 * 3.5)
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 ** produto_2)
