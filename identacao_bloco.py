01831d5fbc499bd2a71afec455c720da9d8846ca
Hugo Sodré <hugosodre031@gmail.com>
2023-08-10 14:55:59 -0300

    identacao_bloco
    Aula sobre identacao de blocos em python

---
+++ identacao_bloco.py
@@ -0,0 +1,12 @@
+
+def sacar(valor):
+    saldo = 500
+    if saldo >= valor:
+        print("Valor sacado!")
+        print("retire seu dinheiro na boca do caixa")
+    print("Obrigado por ser nosso cleinte, tenha um bom dia!")
+
+def depositar (valor) :
+    saldo = 500
+    saldo += valor
+sacar(100)
