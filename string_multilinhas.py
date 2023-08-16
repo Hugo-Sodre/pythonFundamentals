10e81ea78f9e56c53dce6d3ea29e035a89976db1
Hugo Sodré <hugosodre031@gmail.com>
2023-08-15 21:21:55 -0300

    string multilinhas

---
+++ string_multilinhas.py
@@ -0,0 +1,12 @@
+nome = "Hugo"
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+"""
+
+mensagem= f'''
+Olá meu nome é {nome},
+ Eu estou aprendendo Python.
+    Essa mensagem tem diferentes recuos
+'''
